48fbddc345b4ba97a32ddd717dd937ee2ab02d84
Jay Janssen <jay.janssen@gmail.com>
2014-03-13 12:40:42 -0400

    remove old doc

--- modules/pxc/manifests/server.pp
+++ modules/pxc/manifests/server.pp
@@ -1,50 +1,6 @@
 
-# Class: galera::server
-#
-# manages the installation of the mysql wsrep and galera.
-# manages the package, service, wsrep.cnf
-#
-# Parameters:
-#  [*config_hash*]         - hash of config parameters that need to be set.
-#  [*enabled*]             - Defaults to true, boolean to set service ensure.
-#  [*manage_service*]      - Boolean dictating if mysql::server should manage the service.
-#  [*root_group]           - use specified group for root-owned files.
-#  [*package_ensure*]      - Ensure state for package. Can be specified as version.
-#  [*galera_package_name*] - The name of the galera package.
-#  [*wsrep_package_name*]  - The name of the wsrep package.
-#  [*libaio_package_name*] - The name of the libaio package.
-#  [*libssl_package_name*] - The name of the libssl package.
-#  [*wsrep_deb_name*]      - The name of wsrep .deb file.
-#  [*galera_deb_name*]     - The name of galera .deb file.
-#  [*wsrep_deb_name*]      - The URL to download the wsrep .deb file.
-#  [*galera_deb_name*]     - The URL to download the galera .deb file.
-#  [*galera_package_name*] - The name of the Galera package.
-#  [*wsrep_package_name*]  - The name of the WSREP package.
-#  [*cluster_name*]        - Logical cluster name. Should be the same for all nodes.
-#  [*master_ip*]           - IP address of the group communication system handle.
-#    The first node in the cluster should be left as the default (false) until the cluster is formed.
-#    Additional nodes in the cluster should have an IP address set to a node in the cluster.
-#  [*wsrep_sst_username*]  - Username used by the wsrep_sst_auth authentication string.
-#    Used to secure the communication between cluster members.
-#  [*wsrep_sst_password*]  - Password used by the wsrep_sst_auth authentication string.
-#    Used to secure the communication between cluster members.
-#  [*wsrep_sst_method*]    - WSREP state snapshot transfer method.
-#    Defaults to 'mysqldump'.  Note: 'rsync' is the most widely tested.
-#
-# Requires:
-#
-# Sample Usage:
-# class { 'mysql::server::galera':
-#   config_hash => {
-#     'root_password' => 'root_pass',
-#   },
-#    cluster_name       => 'galera_cluster',
-#    master_ip          => false,
-#    wsrep_sst_username => 'ChangeMe',
-#    wsrep_sst_password => 'ChangeMe',
-#    wsrep_sst_method   => 'rsync'
-#  }
-#
+# Class: pxcThis mea::server
+
 class pxc::server (
 	$config_hash      = 	{},
 	$enabled          = 	true,
